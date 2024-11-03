4296d4528ff0782611e61a81f621f9dbd0d55854
mohamed1rebai <mohamed.rebai@esprit.tn>
2024-11-03 17:43:29 +0100

    first commit

---
+++ src/test/java/tn/esprit/spring/services/RegistrationServicesImplTest.java
@@ -0,0 +1,29 @@
+package tn.esprit.spring.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegistrationServicesImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void addRegistrationAndAssignToSkier() {
+    }
+
+    @Test
+    void assignRegistrationToCourse() {
+    }
+
+    @Test
+    void addRegistrationAndAssignToSkierAndCourse() {
+    }
+
+    @Test
+    void numWeeksCourseOfInstructorBySupport() {
+    }
+}
